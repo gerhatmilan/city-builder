fa54299bab76d00d7c6a0fb9226dca9ad530226e
Lszaboo <szabolevi2002@gmail.com>
2023-04-02 16:59:50 +0200

    Alap játékmező megjelenítve

---
+++ SimCity/simcityView/ViewModel/Block.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace simcityView.ViewModel
+{
+    internal class Block : ViewModelBase
+    {
+
+        #region variables
+        
+        public int X;
+        public int Y;
+        private string _floorTexture;
+        private string _buildingTexture;
+
+
+
+        #endregion
+
+        #region props
+        public string FloorTexture
+        {
+            get { return _floorTexture; }
+            set { _floorTexture = value; OnPropertyChanged(nameof(FloorTexture)); }
+        }
+        public string BuildingTexture
+        {
+            get { return _buildingTexture; }
+            set { _buildingTexture = value; OnPropertyChanged(nameof(BuildingTexture)); }
+        }
+
+
+
+        public DelegateCommand? ClickCom { get; set; }
+
+        #endregion
+
+        #region contructor
+
+        public Block()
+        {
+            FloorTexture = @"~\..\View\Textures\missing_texture.png";
+            BuildingTexture = @"~\..\Textures\missing_texture.png";
+        }
+
+        #endregion
+
+        #region functions
+
+       
+        
+
+        #endregion
+
+    }
+}
--- SimCity/simcityView/ViewModel/SimCityViewModel.cs
+++ SimCity/simcityView/ViewModel/SimCityViewModel.cs
@@ -1,18 +1,57 @@
 ﻿using simcityModel.Model;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace simcityView.ViewModel
 {
-    internal class SimCityViewModel
+    internal class SimCityViewModel : ViewModelBase
     {
+        #region variables
+
+
+        private SimCityModel _model;
+
+        #endregion
+
+        #region props
+
+        public ObservableCollection<Block> Cells { get; set; }
+
+        #endregion
+
+        #region constructor
         public SimCityViewModel(SimCityModel model)
         {
-
-
+            _model= model;
+            Cells = new ObservableCollection<Block>();
+            fillCells(10);
         }
+
+        #endregion
+
+        #region functions
+
+        private void fillCells(int size)
+        {
+            Cells.Clear();
+            for(int x = 0; x<size; x++)
+            {
+                for(int y = 0; y<size; y++)
+                {
+                    Block b = new Block();
+                    b.X = x;
+                    b.Y= y;
+                    b.BuildingTexture = @"~\..\Textures\building_medium_blue_a.png";
+                    b.ClickCom = new DelegateCommand(param => { });
+                    Cells.Add(b);
+                }
+            }
+        }
+
+        #endregion
     }
 }
