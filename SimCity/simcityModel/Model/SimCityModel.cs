8068f283b11d622ea395a88392b3cb038bb54740
Gerhát Milán <zbdlrl@inf.elte.hu>
2023-04-02 17:51:14 +0200

    Alap események és eseménykiváltó-metódusok létrehozása a modellben.

--- SimCity/simcityModel/Model/SimCityModel.cs
+++ SimCity/simcityModel/Model/SimCityModel.cs
@@ -8,12 +8,62 @@ using System.Threading.Tasks;
 namespace simcityModel.Model
 {
     public class SimCityModel
-    {
-        public SimCityModel(IDataAcces dataAcces)
+    {  
+        #region Events
+
+        /// <summary>
+        /// Game matrix change event.
+        /// Gets invoked when an element's type in the game matrix changes.
+        /// As a parameter, it passes the changed element's coordinates to the subscriber.
+        /// </summary>
+        public event EventHandler<Tuple<int, int>>? MatrixChanged;
+
+        /// <summary>
+        /// Game advance event.
+        /// Gets invoked every AdvanceTime method is called.
+        /// As a parameter, it passes GameEventArgs to the subscriber, which holds data of the game.
+        /// </summary>
+        public event EventHandler<GameEventArgs>? GameAdvanced;
+
+        /// <summary>
+        /// Game over event.
+        /// Gets invoked when the game is over.
+        /// </summary>
+        public event EventHandler? GameOver;
+
+        #endregion
+
+        public SimCityModel(IDataAcces dataAccess)
         {
 
         }
 
+        #region Private event triggers
+        /// <summary>
+        /// Invoking MatrixChanged event.
+        /// </summary>
+        /// <param name="coordinates">Changed element's coordinates.</param>
+        private void OnMatrixChanged(Tuple<int, int> coordinates)
+        {
+            MatrixChanged?.Invoke(this, coordinates);
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        private void OnGameAdvanced()
+        {
+            GameAdvanced?.Invoke(this, new GameEventArgs(_gameTime, _money, _population));
+        }
+
+        /// <summary>
+        /// Invoking GameOver event.
+        /// </summary>
+        private void OnGameOver()
+        {
+            GameOver?.Invoke(this, new EventArgs());
+        }
+
+        #endregion
     }
 }
