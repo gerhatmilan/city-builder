6373bfc670096751c0ae7ba397f229486f186023
Gerhát Milán <zbdlrl@inf.elte.hu>
2023-04-07 17:52:36 +0200

    Saját kivételosztály hozzáadása az építkezés közben felmerülő problémák kezelésére.

---
+++ SimCity/simcityModel/Model/CannotBuildException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simcityModel.Model
+{
+    /// <summary>
+    /// Custom exception class for build actions.
+    /// </summary>
+    public class CannotBuildException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of CannotBuildException class.
+        /// </summary>
+        /// <param name="message">The message of the exception.</param>
+        public CannotBuildException(string message) : base(message) { }
+    }
+}
