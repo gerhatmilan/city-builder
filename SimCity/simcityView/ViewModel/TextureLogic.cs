aadaa64eb98e65cbe7564997e37a08dd906d3813
Levente Zoltán Szabó <ecnq22@INF.ELTE.HU>
2023-05-09 23:07:22 +0200

    Textúrázási logika kigondolva, előkészítve!

---
+++ SimCity/simcityView/ViewModel/TextureLogic.cs
@@ -0,0 +1,29 @@
+﻿using simcityModel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace simcityView.ViewModel
+{
+    internal interface TextureLogic
+    {
+        //Idea: Use theese classes from the TextureManager, where we give an enum a texturing logic!
+        //These classes modify each Block in the ViewModel according to the given logic.
+        
+        #region Props
+        public ImageBrush[] FloorTextures { get; set;  }
+        public BitmapImage[] BuildingTextures { get; set; }
+        public SimCityModel Model { get; set; }
+        #endregion
+        #region Texturing functions
+        public void SetLogicalAllTextures();
+        public void SetLogicalFloorTextures();
+        public void SetLogicalBuildingTextures();
+        public void UpdateWithLogicalTexture();
+        #endregion
+    }
+}
